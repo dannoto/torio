cfe1671a8ee4cc0fe5213ba11968af86d13ab801
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-09-24 01:01:27 -0300

    .

--- application/views/conta/campanhas.php
+++ application/views/conta/campanhas.php
@@ -160,10 +160,10 @@
                       </td>
                       <td>
                       <div class="d-flex align-items-center justify-content-center">
-                          <span class="me-2 text-xs font-weight-bold"><?php echo ( count($this->api_model->dash_get_oferta_concluidas_by_campanha($p->id)) / count($this->api_model->dash_get_demandas_abertas($t->id), 1))?>%</span>
+                          <span class="me-2 text-xs font-weight-bold"><?php echo ( count($this->api_model->dash_get_oferta_concluidas_by_campanha($p->id)) / count($this->api_model->dash_get_demandas_abertas($t->id)) ) ; ?>%</span>
                           <div>
                             <div class="progress">
-                              <div class="progress-bar bg-gradient-success" role="progressbar" aria-valuenow="<?php echo ( count($this->api_model->dash_get_oferta_concluidas_by_campanha($p->id)) / count($this->api_model->dash_get_demandas_abertas($t->id), 1))?>" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
+                              <div class="progress-bar bg-gradient-success" role="progressbar" aria-valuenow="<?php echo ( count($this->api_model->dash_get_oferta_concluidas_by_campanha($p->id)) / count($this->api_model->dash_get_demandas_abertas($t->id)) );?>" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
                             </div>
                           </div>
                         </div>
