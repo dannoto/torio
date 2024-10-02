6db478bf15407c0620473d24112885478486c9bc
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-10-02 16:27:14 -0300

    .

--- application/models/Conta_model.php
+++ application/models/Conta_model.php
@@ -857,8 +857,8 @@ class Conta_model extends CI_Model
         $this->db->where('oferta_produto_id', $oferta_produto_id);
         $this->db->where('oferta_tag_id', $oferta_tag_id);
         $this->db->where('oferta_tipo', $oferta_tipo);
-        $this->db->where('oferta_status', 0);
-        $this->db->where('oferta_status', 1);
+        // $this->db->where('oferta_status', 0);
+        // $this->db->where('oferta_status', 1);
 
         // $campanha->id, $campanha->campanha_produto_id, $campanha->campanha_tag_id, 'sms', $persona->id
         
