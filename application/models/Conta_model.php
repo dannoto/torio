def998fb70a4a5edf05e2f2e32d83ca740d6ff87
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-10-04 15:59:27 -0300

    .

--- application/models/Conta_model.php
+++ application/models/Conta_model.php
@@ -1114,10 +1114,13 @@ class Conta_model extends CI_Model
             
             foreach ($duplicate_ips as $ip) {
                 // Mantém apenas o primeiro registro e deleta os duplicados
-               $D = $this->db->query('DELETE FROM cliques WHERE clique_ip = ? AND id NOT IN (SELECT id FROM (SELECT MIN(id) as id FROM cliques WHERE clique_ip = ?) as temp);', [$ip->clique_ip, $ip->clique_ip]);
+               $D = $this->db->query("DELETE FROM cliques WHERE clique_ip = ".$ip->clique_ip."");
                 
                if ($D) {
                     echo "EXCLUIDO ".$ip->clique_ip."<br>";
+               } else{
+                echo "ERRO AO EXCLUIR ".$ip->clique_ip."<br>";
+
                }
             
             }
