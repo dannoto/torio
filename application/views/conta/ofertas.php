c084902432a690ee750c9a24f66c7bd60715e66a
Daniel Ribeiro <sdanrib2018@gmail.com>
2025-07-09 17:50:39 -0300

    .

--- application/views/conta/ofertas.php
+++ application/views/conta/ofertas.php
@@ -212,7 +212,7 @@
 
                         <td>
                           <div class="d-flex px-2">
-                            <button class="btn bg-gradient-primary" onclick="delete_oferta(<?= $p->id ?>)">X</button>
+                            <button class="btn bg-gradient-primary" style="width:90%" onclick="delete_oferta(<?= $p->id ?>)">X</button>
                           </div>
                         </td>
 
