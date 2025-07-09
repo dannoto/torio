0ff6fb8dd4fe1c4b1fa4ccb9cded48b73c079e4d
Daniel Ribeiro <sdanrib2018@gmail.com>
2025-07-09 19:34:01 -0300

    mercadolivre

--- application/controllers/Conta.php
+++ application/controllers/Conta.php
@@ -483,7 +483,7 @@ class Conta extends CI_Controller
 
             $config['base_url'] = base_url('conta/ofertas');
             $config['total_rows'] = $this->conta_model->count_ofertas(); // Total de registros
-            $config['per_page'] = 10; // Quantidade de imóveis por página
+            $config['per_page'] = 30; // Quantidade de imóveis por página
             $config['uri_segment'] = 3; // Segmento da URL onde a página está indicada
 
             $config['full_tag_open'] = '<ul class="pagination">';
