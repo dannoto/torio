f05d7fd836153585fc6be92dd9206eab553f8866
Daniel Ribeiro <sdanrib2018@gmail.com>
2025-07-09 17:47:16 -0300

    .

--- application/views/conta/ofertas.php
+++ application/views/conta/ofertas.php
@@ -181,7 +181,7 @@
                           ?>
 
                           <!-- Ícone de copiar -->
-                          <button onclick="copiarOferta('<?= $ofertaId ?>')" style="cursor: pointer; background-color: #007bff;width:90%" title="Copiar oferta">
+                          <button onclick="copiarOferta('<?= $ofertaId ?>')" style="cursor: pointer; background-color: #007bff;width:90%;color:#FFF;border:1px solid #007bff" title="Copiar oferta">
                              CC
                           </button>
 
