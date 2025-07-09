6c1b8968bd912c662abc94372522fc605517cc4d
Daniel Ribeiro <sdanrib2018@gmail.com>
2025-07-09 19:33:28 -0300

    mercadolivre

--- application/controllers/Conta.php
+++ application/controllers/Conta.php
@@ -450,7 +450,7 @@ class Conta extends CI_Controller
 
             $config['base_url'] = $base_url;
             $config['total_rows'] = $this->conta_model->count_search_ofertas($f_data);
-            $config['per_page'] = 10;
+            $config['per_page'] = 30;
             $config['uri_segment'] = 3;
 
             $config['full_tag_open'] = '<ul class="pagination">';
