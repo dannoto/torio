f20fb6484758838364ad55b73ac7d8bb1d946631
Daniel Ribeiro <sdanrib2018@gmail.com>
2025-07-09 19:04:51 -0300

    mercadolivre

--- application/views/conta/ofertas.php
+++ application/views/conta/ofertas.php
@@ -202,7 +202,7 @@
                         <td>
                          
                           <!-- Ícone de copiar -->
-                          <button onclick="setEnviado('<?= $P->ID ?>')" style="cursor: pointer; background-color:rgb(244, 11, 11);width:90%;color:#FFF;border:1px solid rgb(244, 11, 11)" title="Copiar oferta">
+                          <button onclick="setEnviado('<?= $P->id ?>')" style="cursor: pointer; background-color:rgb(244, 11, 11);width:90%;color:#FFF;border:1px solid rgb(244, 11, 11)" title="Copiar oferta">
                             CONCLUÍDO
                           </button>
 
